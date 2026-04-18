7a815c64b7bbda3e2ecf6b182e968d85abea813d
yarrib <yarrib@users.noreply.github.com>
2026-04-18 16:27:35 +0000

    deploy: 7f7b8e0474e26484e135c457e7acbe0823adeed6

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-7f47b6b7.js',
+                path_to_root + 'searchindex-549b00a0.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
