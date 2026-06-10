eb300b8b280c6964fcfc0f665b9eb563675f934d
yarrib <yarrib@users.noreply.github.com>
2026-06-10 18:13:08 +0000

    deploy: 900b30f663b8eb803d68740e970efb9a63168b29

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-946acc84.js',
+                path_to_root + 'searchindex-f608d3fd.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
